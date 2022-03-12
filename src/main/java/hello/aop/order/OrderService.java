ccf4e419a2ff74a7f4da5a7101844b0d543d5d40
monkeyDugi <lbd4946@gmail.com>
2022-03-12 12:45:42 +0900

    feat : 스프링 AOP 구현 시작

--- src/main/java/hello/aop/order/OrderService.java
+++ src/main/java/hello/aop/order/OrderService.java
@@ -12,6 +12,7 @@ public class OrderService {
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
     }
+
     public void orderItem(String itemId) {
         log.info("[orderService] 실행");
         orderRepository.save(itemId);
