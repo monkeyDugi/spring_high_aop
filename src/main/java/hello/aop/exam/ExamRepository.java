2ac91a32772aaa8c10ce8c2691b63e86bfc644c8
monkeyDugi <lbd4946@gmail.com>
2022-03-13 11:46:45 +0900

    feat : 실전 예제 - 재시도 AOP

--- src/main/java/hello/aop/exam/ExamRepository.java
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,8 +1,11 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+@Slf4j
 @Repository
 public class ExamRepository {
 
@@ -12,8 +15,10 @@ public class ExamRepository {
      * 5번에 1번 실패하는 요청
      */
     @Trace
+    @Retry(4)
     public String save(String itemId) {
         seq++;
+        log.info("[reqpository] seq={}", seq);
         if (seq % 5 == 0) {
             throw new IllegalStateException("예외 발생");
         }
